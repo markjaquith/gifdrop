c4ca58b84ce27fd30c8bc8580570b8f8fdbcdb0a
Mark Jaquith <mark@jaquith.me>
2014-05-18 14:20:37 -0400

    Add jQuery Colorbox

--- classes/plugin.php
+++ classes/plugin.php
@@ -114,8 +114,9 @@ class GifDrop_Plugin {
 	}
 
 	protected function register_frontend_scripts() {
-		wp_register_script( 'gifdrop-isotope', $this->get_url() . 'js/isotope.min.js', array(), '2.0.0' );
-		wp_register_script( 'gifdrop', $this->get_url() . 'js/gifdrop.js', array( 'jquery', 'backbone', 'wp-backbone', 'wp-util', 'wp-plupload', 'gifdrop-isotope' ), '0.1' );
+		wp_register_script( 'gifdrop-isotope', $this->get_url() . 'js/isotope.min.js', array('jquery'), '2.0.0' );
+		wp_register_script( 'gifdrop-colorbox', $this->get_url() . 'js/jquery.colorbox.min.js', array('jquery'), '1.5.9' );
+		wp_register_script( 'gifdrop', $this->get_url() . 'js/gifdrop.js', array( 'jquery', 'backbone', 'wp-backbone', 'wp-util', 'wp-plupload', 'gifdrop-isotope', 'gifdrop-colorbox' ), '0.1' );
 	}
 
 	protected function register_frontend_styles() {
