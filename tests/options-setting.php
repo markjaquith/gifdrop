6eba669438f78ab8ad8f8444dcad7bfd3b32e621
Mark Jaquith <mark@jaquith.me>
2014-10-11 05:29:01 -0400

    Test passing in a default value to ->get_option()

--- tests/options-setting.php
+++ tests/options-setting.php
@@ -7,4 +7,8 @@ class GifDrop_Options_Setting extends GifDrop_TestCase {
 		$p->set_option( 'does_not_exist', 'does now!' );
 		$this->assertEquals( 'does now!', $p->get_option( 'does_not_exist' ) );
 	}
+
+	function test_default_option() {
+		$this->assertEquals( 123, $this->plugin()->get_option( 'blah_fake', 123 ) );
+	}
 }
