e77a8f7d2e0db8cc20312ce24011a6a1dd62db5e
Mark Jaquith <mark@jaquith.me>
2014-10-12 15:00:05 -0400

    `<button type="button">` isn't actually redundant.
    (But `<button type="submit">` would be!)
    
    Partially reverts e181a356d8d
    
    See https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button

--- templates/backbone.php
+++ templates/backbone.php
@@ -18,7 +18,7 @@
 	<div class="modal-content-inner">
 		<div data-clipboard-text="{{data.src}}" class="copy-to-clipboard" style="width:{{data.width}}px">
 			<p><img src="{{data.src}}" width="{{data.width}}" height="{{data.height}}" /></p>
-			<button class="copy" data-copied-message="<?php esc_attr_e( 'Copied!', 'gifdrop' ); ?>"><?php _e( 'Copy URL', 'gifdrop' ); ?></button>
+			<button type="button" class="copy" data-copied-message="<?php esc_attr_e( 'Copied!', 'gifdrop' ); ?>"><?php _e( 'Copy URL', 'gifdrop' ); ?></button>
 		</div>
 		<p class="details">
 			<i class="dashicons dashicons-tag"></i><input class="title" type="text" value="{{data.title}}" />
